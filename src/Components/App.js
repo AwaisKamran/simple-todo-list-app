7346cdd9b40f3df157ce0d5a4b15764de1a13a47
Awais Kamran <awaiskamran@Awaiss-MacBook-Air.local>
2022-08-13 18:24:27 +0500

    feat: applied styles to the view component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import axios from "axios";
-import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 
 class App extends React.Component{
   constructor(props){
@@ -15,8 +14,9 @@ class App extends React.Component{
     this.getData();
   }
 
-  getData(){
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((response) => {
+  getData() {
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => {
       this.setState({ data: response.data})
     });
   }
@@ -27,13 +27,30 @@ class App extends React.Component{
         <div className="todo-list-container">
           <div className="input-container">
             <input className="input-field" type="text" placeholder="Enter task here" />
-            <button className="add-button">Add</button>
+            <button className="add-button">
+              <span className="material-symbols-outlined add-icon">add</span>
+            </button>
           </div>
   
           <div className="data-container">
             {
-              this.state.data.map((todo) => (<div>{todo.title}</div>))
+              this.state.data.map((todo) => (
+                <div className="todo-item-container">
+                  <input type="checkbox" />
+                  <div className="todo-item" key={todo.id}>
+                    {todo.title}
+                  </div>
+                </div>
+                )
+              )
             }
+          </div>
+
+          <div className='button-container'>
+            <button>
+              <span className="delete-text">Delete</span> 
+              <span className="material-symbols-outlined delete-icon">delete</span>
+            </button>
           </div>
         </div>
       </div>
