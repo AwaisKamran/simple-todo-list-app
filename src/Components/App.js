e1d373bb02b6202c284cb9b633fdbba4d8d5ea53
Awais Kamran <awais.kamran@taxfix.de>
2022-08-20 17:36:32 +0500

    feat: class component data events added.

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './App.css';
 import axios from "axios";
 
+const ENTER_KEY = 13;
 class App extends React.Component{
   constructor(props){
     super(props);
+    this.textInput = React.createRef();
     this.state = {
       data: []
     };
   }
 
   componentDidMount(){
-    this.getData();
+    //this.getData();
+    this.textInput.current.focus();
   }
 
   getData() {
@@ -21,28 +24,53 @@ class App extends React.Component{
     });
   }
 
+  addItem = () => {
+    const title = this.textInput.current.value;
+    if(title){
+      const data = [... this.state.data];
+
+      data.push({
+        id: data.length + 1,
+        completed: false,
+        userId: 10,
+        title,
+      });
+  
+      this.setState({ data })
+    }
+  }
+
+  detectKey = (event) => {
+    if(event.charCode === ENTER_KEY){
+      this.addItem()
+      this.textInput.current.value = "";
+    }
+  }
+
   render(){
     return (
       <div className="container">
+        <div className='heading'>Todo List</div>
         <div className="todo-list-container">
           <div className="input-container">
-            <input className="input-field" type="text" placeholder="Enter task here" />
-            <button className="add-button">
+            <input onKeyPress={this.detectKey} ref={this.textInput} className="input-field" type="text" placeholder="Enter task here" />
+            <button className="add-button" onClick={this.addItem}>
               <span className="material-symbols-outlined add-icon">add</span>
             </button>
           </div>
   
           <div className="data-container">
             {
-              this.state.data.map((todo) => (
-                <div className="todo-item-container">
-                  <input type="checkbox" />
-                  <div className="todo-item" key={todo.id}>
-                    {todo.title}
+              this.state.data.length > 0 ? 
+                this.state.data.map((todo) => (
+                  <div key={todo.id} className="todo-item-container">
+                    <input type="checkbox" />
+                    <div className="todo-item">
+                      {todo.title}
+                    </div>
                   </div>
-                </div>
-                )
-              )
+                  )
+                ): <div className="no-data">No data available</div>
             }
           </div>
 
