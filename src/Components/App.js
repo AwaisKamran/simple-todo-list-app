43ad944c2abcbbbf8ca4c2e1ad8485867d39a472
javedbaloch4 <mjavedahmed@live.com>
2022-08-23 03:04:22 +0500

    Fix space string entry

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,56 +1,56 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import axios from "axios";
 import Heading from "./Heading/Heading";
-import DataContainer from "./DataContainer/DataContainer"
-import InputContainer from './InputContainer/InputContainer';
-import ActionContainer from './ActionContainer/ActionContainer';
+import DataContainer from "./DataContainer/DataContainer";
+import InputContainer from "./InputContainer/InputContainer";
+import ActionContainer from "./ActionContainer/ActionContainer";
 
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     //this.getData();
   }
 
   getData() {
-    axios.get("https://jsonplaceholder.typicode.com/todos")
-    .then((response) => {
-      this.setState({ data: response.data})
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((response) => {
+      this.setState({ data: response.data });
     });
   }
 
   addItem = (title) => {
-    if(title){
-      const data = [... this.state.data];
-
-      data.push({
-        id: data.length + 1,
-        completed: false,
-        userId: 10,
-        title,
-      });
-  
-      this.setState({ data })
+    if (title.trim().length === 0) {
+      return false;
     }
-  }
+
+    const data = [...this.state.data];
+
+    data.push({
+      id: data.length + 1,
+      completed: false,
+      userId: 10,
+      title,
+    });
+
+    this.setState({ data });
+  };
 
   deleteItem = (items) => {
-    if(items.length > 0){
-      const data = [... this.state.data];
+    if (items.length > 0) {
+      const data = [...this.state.data];
       // const newData = data.filter((todo) => {
-        
+
       // })
     }
-  }
+  };
 
-
-  render(){
+  render() {
     console.log("App Component Rendered");
     return (
       <div className="container">
@@ -58,7 +58,7 @@ class App extends React.Component{
         <div className="todo-list-container">
           <InputContainer addItem={this.addItem}></InputContainer>
           <DataContainer data={this.state.data}></DataContainer>
-          <ActionContainer></ActionContainer> 
+          <ActionContainer></ActionContainer>
         </div>
       </div>
     );
